b0346164ae44b7bf751222e509a3725ee6d705be
tairoroberto <redhotTairo1507!github>
2016-12-09 15:45:52 -0200

    Adjusting provider

--- app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -17,12 +17,15 @@ package android.example.com.dictionaryproviderexample;
 
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.provider.UserDictionary.Words;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.ListView;
-import android.support.v4.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 /**
  * This is the central activity for the Provider Dictionary Example App. The purpose of this app is
@@ -30,40 +33,63 @@ import android.support.v4.widget.SimpleCursorAdapter;
  */
 public class MainActivity extends ActionBarActivity {
 
-    // For the SimpleCursorAdapter to match the UserDictionary columns to layout items.
-    private static final String[] COLUMNS_TO_BE_BOUND  = new String[] {
-            UserDictionary.Words.WORD,
-            UserDictionary.Words.FREQUENCY
+    private static final String TAG = "LOG";
+
+    private static final String[] COLUMNS_TO_BE_BOUND = new String[]{
+        Words.WORD,
+        Words.FREQUENCY
     };
 
-    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[] {
-            android.R.id.text1,
-            android.R.id.text2
+    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[]{
+        android.R.id.text1,
+        android.R.id.text2
     };
 
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Get the TextView which will be populated with the Dictionary ContentProvider data.
+        TextView dictTextView = (TextView) findViewById(R.id.dictionary_text_view);
         ListView dictListView = (ListView) findViewById(R.id.dictionary_list_view);
 
-        // Get the ContentResolver which will send a message to the ContentProvider.
+        // Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver = getContentResolver();
 
-        // Get a Cursor containing all of the rows in the Words table.
+        // Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
 
-        // Set the Adapter to fill the standard two_line_list_item layout with data from the Cursor.
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+        try {
+            dictTextView.setText("The UserDictionary contains " + cursor.getCount() + " words \n");
+            dictTextView.append("COLUMNS: " + Words._ID + " - " + Words.FREQUENCY + " - " + Words.WORD);
+
+            int idColumn = cursor.getColumnIndex(UserDictionary.Words._ID);
+            int frequencyColumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
+            int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
+
+            while (cursor.moveToNext()){
+                int id = cursor.getInt(idColumn);
+                int frequency = cursor.getInt(frequencyColumn);
+                String word = cursor.getString(wordColumn);
+
+                dictTextView.append("\n" + id + " - " + frequency + " - " + word);
+            }
+
+        } finally {
+            cursor.close();
+        }
+
+        cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(
+                MainActivity.this,
                 android.R.layout.two_line_list_item,
                 cursor,
                 COLUMNS_TO_BE_BOUND,
                 LAYOUT_ITEMS_TO_FILL,
-                0);
-
-        // Attach the adapter to the ListView.
-        dictListView.setAdapter(adapter);
+                0
+        );
+        dictListView.setAdapter(cursorAdapter);
     }
 }
